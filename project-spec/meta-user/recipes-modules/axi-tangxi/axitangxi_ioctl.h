27baf9712365a8f0dd9130c2db18a48ad8bab4cd
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-11-08 18:22:19 +0800

    :truck: Move a struct

--- project-spec/meta-user/recipes-modules/axi-tangxi/axitangxi_ioctl.h
+++ project-spec/meta-user/recipes-modules/axi-tangxi/axitangxi_ioctl.h
@@ -21,6 +21,27 @@
 // The default timeout for DMA is 10 seconds
 #define AXI_TANGXI_TIMEOUT 10000
 
+// 内核参数定义
+struct axitangxi_transfer {
+  // struct completion comp;         // A completion to use for waiting
+
+  // 必须配置
+  size_t tx_data_size; // 发送数据大小
+  size_t rx_data_size; // 接收数据大小
+
+  size_t burst_size;  // 突发长度
+  size_t burst_count; // 突发次数
+  size_t burst_data;  // 一次突发数据大小
+
+  uint32_t tx_data_ps_ptr; // 突发发送地址(PS DDR地址)
+  uint32_t rx_data_ps_ptr; // 突发接收地址(PS DDR地址)
+
+  uint32_t tx_data_pl_ptr; // 突发发送地址(PL DDR地址)
+  uint32_t rx_data_pl_ptr; // 突发接收地址(PL DDR地址)
+
+  uint8_t node; // 芯片node节点
+};
+
 struct axitangxi_transaction {
   // 必须配置
   size_t tx_data_size; // 发送数据大小
