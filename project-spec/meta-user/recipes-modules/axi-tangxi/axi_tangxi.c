9ba95bfcf434fa83c454522d9dee50203cd498a3
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-11-08 18:22:19 +0800

    :truck: Move a struct

--- project-spec/meta-user/recipes-modules/axi-tangxi/axi_tangxi.c
+++ project-spec/meta-user/recipes-modules/axi-tangxi/axi_tangxi.c
@@ -16,9 +16,6 @@
 #include "axitangxi_dev.h"
 #include "axitangxi_ioctl.h" // IOCTL interface definition and types
 
-// The default timeout for axi-tangxi is 10 seconds
-#define AXI_TANGXI_TIMEOUT 10000
-
 // 引入需要映射的寄存器虚拟地址定义
 extern void __iomem *dma_mm2s_ps_addr;
 extern void __iomem *dma_mm2s_ps_size;
@@ -48,27 +45,6 @@ extern void __iomem *network_acc_trans_addr;
 extern void __iomem *network_acc_trans_size;
 extern void __iomem *network_acc_entropy_addr;
 extern void __iomem *network_acc_entropy_size;
-
-// 内核参数定义
-struct axitangxi_transfer {
-  // struct completion comp;         // A completion to use for waiting
-
-  // 必须配置
-  size_t tx_data_size; // 发送数据大小
-  size_t rx_data_size; // 接收数据大小
-
-  size_t burst_size;  // 突发长度
-  size_t burst_count; // 突发次数
-  size_t burst_data;  // 一次突发数据大小
-
-  uint32_t tx_data_ps_ptr; // 突发发送地址(PS DDR地址)
-  uint32_t rx_data_ps_ptr; // 突发接收地址(PS DDR地址)
-
-  uint32_t tx_data_pl_ptr; // 突发发送地址(PL DDR地址)
-  uint32_t rx_data_pl_ptr; // 突发接收地址(PL DDR地址)
-
-  uint8_t node; // 芯片node节点
-};
 
 // 初始化中断参数
 struct axitangxi_irq_data axitangxi_irq_data = {0};
