145954d9e663dc56905d028123c28e944971de1a
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-11-29 13:07:02 +0800

    :sparkless: Add global status

--- project-spec/meta-user/recipes-apps/autostart/src/main.c
+++ project-spec/meta-user/recipes-apps/autostart/src/main.c
@@ -3,21 +3,29 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 #include "axitangxi.h"
 #include "coding.h"
 #include "config.h"
 #include "main.h"
-#include "transmission_protocol.h"
 #include "utils.h"
 
-// 权重、因子、图片的地址大小
+#define PICTURES_NUMBER_MAX 8
+// 权重、因子、图片的地址
 #define WEIGHT_ADDR 0x10000000
 #define QUANTIFY_ADDR 0x10010000
 #define PICTURE_BASE_ADDR 0x20000000
+
+// request status will return it.
+status_t status;
+// picture number
+size_t number;
+data_t bit_streams[PICTURES_NUMBER_MAX];
 
 static void init_opt(opt_t *opt) {
   opt->tty = "/tmp/ttyS1";
@@ -64,12 +72,73 @@ static int parse(int argc, char *argv[], opt_t *opt) {
   return 0;
 }
 
+size_t process_data_frames(int fd, data_frame_t *input_data_frames,
+                           n_frame_t n_frame, struct network_acc_reg reg,
+                           uint8_t *addr) {
+  // convert data frames to yuv
+  ssize_t yuv_len = data_frame_to_data_len(input_data_frames, n_frame);
+  data_t yuv[3] = {};
+  yuv[0].addr = ps_mmap(fd, yuv_len);
+  data_frames_to_data(input_data_frames, n_frame, yuv[0].addr);
+  yuv[2].len = data_to_yuv420(yuv[0].addr, &yuv[1].addr, &yuv[2].addr, yuv_len);
+  yuv[1].len = yuv[2].addr - yuv[1].addr;
+  yuv[0].len = yuv[1].addr - yuv[0].addr;
+
+  // neural encoding yuv
+  data_t trans[3] = {};
+  data_t entropy[3] = {};
+  for (int k = 0; k < 3; k++) {
+    if (pl_write(fd, yuv[k].addr, reg.picture_addr = PICTURE_BASE_ADDR,
+                 reg.picture_size = yuv[k].len) == -1)
+      err(errno, NULL);
+
+    status |= TP_STATUS_NETWORK_ENCODING;
+    pl_run(fd, &reg);
+    status &= ~TP_STATUS_NETWORK_ENCODING;
+
+    status |= TP_STATUS_NETWORK_ENCODING;
+    // TODO: multithread
+    pl_get(fd, &reg, (uint16_t *)trans[k].addr, (uint16_t *)entropy[k].addr);
+    status &= ~TP_STATUS_NETWORK_ENCODING;
+
+    trans[k].len = reg.trans_size;
+    entropy[k].len = reg.entropy_size;
+  }
+  munmap(yuv[0].addr, yuv_len);
+
+  // entropy encoding y' u' v'
+  data_t data[3] = {};
+  size_t len = 0;
+  status |= TP_STATUS_ENTROPY_ENCODING;
+  for (int k = 0; k < 3; k++) {
+    gmm_t gmm;
+    entropy_to_gmm((uint16_t *)entropy[k].addr, &gmm);
+
+    // TODO: multithread
+    data[k].len =
+        coding(gmm, (uint16_t *)trans[k].addr, trans[k].len, data[k].addr);
+
+    len += data[k].len;
+  }
+  status &= ~TP_STATUS_ENTROPY_ENCODING;
+  // combine 3 channels to one
+  if (addr == NULL)
+    addr = malloc(len);
+  uint8_t *p = addr;
+  for (int k = 0; k < 3; k++) {
+    memcpy(p, data[k].addr, data[k].len);
+    p += data[k].len;
+    free(data[k].addr);
+  }
+  return len;
+}
+
 int main(int argc, char *argv[]) {
   opt_t opt;
-  int status = parse(argc, argv, &opt);
-  if (status == -1)
+  int ret = parse(argc, argv, &opt);
+  if (ret == -1)
     errx(EXIT_FAILURE, "parse failure!");
-  else if (status > 0)
+  else if (ret > 0)
     return EXIT_SUCCESS;
 
   // configure device
@@ -82,75 +151,103 @@ int main(int argc, char *argv[]) {
   int fd = open(opt.tty, O_RDWR | O_NOCTTY);
   if (fd == -1)
     err(errno, "%s", opt.tty);
-  frame_t input_frame;
+  frame_t input_frame, output_frame = {.address = TP_ADDRESS_SLAVE};
   ssize_t n;
-  while (1) {
+  while (true) {
     do {
       n = receive_frame(fd, &input_frame);
-    } while (n <= 0);
-    frame_t output_frame = {
-        .address = TP_ADDRESS_SLAVE,
-        .frame_type = TP_FRAME_TYPE_NACK,
-        .n_file = input_frame.n_file,
-        .n_frame = input_frame.n_frame,
-    };
-    send_frame(fd, &output_frame);
-    output_frame.frame_type = TP_FRAME_TYPE_ACK;
-    send_frame(fd, &output_frame);
-    data_frame_t *input_data_frames =
-        malloc(input_frame.n_frame * sizeof(data_frame_t));
-    if (input_data_frames == NULL)
-      err(errno, NULL);
-    for (int i = 0; i < input_frame.n_frame; i++)
-      do {
-        n = receive_data_frame(fd, &input_data_frames[i]);
-      } while (n <= 0);
-    ssize_t yuv_len =
-        data_frame_to_data_len(input_data_frames, input_frame.n_frame);
-    data_t yuv[3] = {};
-    yuv[0].addr = ps_mmap(fd_dev, yuv_len);
-    data_frames_to_data(input_data_frames, input_frame.n_frame, yuv[0].addr,
-                        yuv_len);
-    free(input_data_frames);
-    yuv[2].len = data_to_yuv420(yuv[0].addr, yuv[1].addr, yuv[2].addr, yuv_len);
-    yuv[1].len = yuv[2].addr - yuv[1].addr;
-    yuv[0].len = yuv[1].addr - yuv[0].addr;
+    } while (n <= 0 || input_frame.address != TP_ADDRESS_MASTER);
 
-    for (int k = 0; k < 3; k++) {
-      if (pl_write(fd_dev, yuv[k].addr, reg.picture_addr = PICTURE_BASE_ADDR,
-                   reg.picture_size = yuv[k].len) == -1)
+    switch (input_frame.frame_type) {
+    case TP_FRAME_TYPE_QUERY:
+      output_frame.frame_type = input_frame.frame_type;
+      output_frame.status = status;
+      send_frame(fd, &output_frame);
+      break;
+
+    case TP_FRAME_TYPE_SEND:
+      output_frame.frame_type = input_frame.frame_type;
+      output_frame.n_file = input_frame.n_file;
+      output_frame.n_frame = input_frame.n_frame;
+      send_frame(fd, &output_frame);
+      data_frame_t *input_data_frames =
+          malloc(input_frame.n_frame * sizeof(data_frame_t));
+      if (input_data_frames == NULL)
         err(errno, NULL);
-
-      pl_run(fd_dev, &reg);
-      uint16_t *trans_addr = NULL, *entropy_addr = NULL;
-      pl_get(fd_dev, &reg, trans_addr, entropy_addr);
-
-      gmm_t gmm;
-      entropy_to_gmm(entropy_addr, &gmm);
-      uint8_t *bits = NULL;
-      size_t bits_len = coding(gmm, trans_addr, reg.trans_size, bits);
-      output_frame.frame_type = TP_FRAME_TYPE_REQUEST;
-      output_frame.n_frame = (bits_len - 1) / TP_FRAME_DATA_LEN_MAX + 1;
+      data_frame_t data_frame;
+      // TODO: timeout
       do {
-        n = receive_frame(fd, &input_frame);
-      } while (n <= 0);
+        n = receive_data_frame(fd, &data_frame);
+      } while (n <= 0 || data_frame.n_file != input_frame.n_file ||
+               data_frame.n_frame >= input_frame.n_frame);
+      memcpy(&input_data_frames[data_frame.n_frame], &data_frame,
+             sizeof(data_frame));
+      for (int i = 0; i < input_frame.n_frame; i++)
+        // TODO: resend
+
+        if (number >= PICTURES_NUMBER_MAX)
+          fprintf(stderr, "picture number exceeds maximum: %d\n",
+                  PICTURES_NUMBER_MAX);
+        else {
+          // TODO: multithread
+          bit_streams[number].len = process_data_frames(
+              fd_dev, input_data_frames, input_frame.n_frame, reg,
+              bit_streams[number].addr);
+          number++;
+        }
+      free(input_data_frames);
+      break;
+
+    case TP_FRAME_TYPE_RECV:
+      if (input_frame.n_file >= PICTURES_NUMBER_MAX) {
+        fprintf(stderr, "picture number exceeds maximum: %d\n",
+                PICTURES_NUMBER_MAX);
+        break;
+      }
+      output_frame.frame_type = input_frame.frame_type;
+      output_frame.n_frame =
+          (bit_streams[number].len - 1) / TP_FRAME_DATA_LEN_MAX + 1;
       send_frame(fd, &output_frame);
       data_frame_t *output_data_frames =
           malloc(output_frame.n_frame * sizeof(data_frame_t));
-      data_to_data_frames(bits, bits_len, output_data_frames,
-                          output_frame.n_frame);
+      // data, data_len and check_sum should be set in data_to_data_frames()
+      for (int i = 0; i < output_frame.n_frame; i++) {
+        memcpy(output_data_frames[i].header, tp_header, sizeof(tp_header));
+        output_data_frames[i].n_file = input_frame.n_file;
+        output_data_frames[i].n_frame = i;
+      }
+      data_to_data_frames(bit_streams[number].addr, bit_streams[number].len,
+                          output_data_frames);
       for (int i = 0; i < output_frame.n_frame; i++)
         send_data_frame(fd, &output_data_frames[i]);
-      while (1) {
+      bool cont = true;
+      while (cont) {
         do {
           n = receive_frame(fd, &input_frame);
-        } while (n <= 0);
-        if (input_frame.frame_type == TP_FRAME_TYPE_NACK)
-          send_data_frame(fd, &output_data_frames[input_frame.n_frame]);
-        else if (input_frame.frame_type == TP_FRAME_TYPE_ACK) {
+        } while (n <= 0 || input_frame.address != TP_ADDRESS_MASTER);
+        switch (input_frame.frame_type) {
+        case TP_FRAME_TYPE_ACK:
+          cont = false;
+          free(output_data_frames);
           break;
+
+        case TP_FRAME_TYPE_NACK:
+          send_data_frame(fd, &output_data_frames[input_frame.n_frame]);
+          break;
+
+        default:
+          fputs("Send ACK/NACK type frame, please!", stderr);
         }
+        break;
       }
+
+    case TP_FRAME_TYPE_ACK:
+    case TP_FRAME_TYPE_NACK:
+      fputs("Send receive type frame firstly!", stderr);
+      break;
+
+    default:
+      fprintf(stderr, "Unknown frame type: %d\n", input_frame.frame_type);
     }
   }
 
--- project-spec/meta-user/recipes-apps/autostart/src/transmission_protocol.h
+++ project-spec/meta-user/recipes-apps/autostart/src/transmission_protocol.h
@@ -8,28 +8,40 @@
 __BEGIN_DECLS
 
 #define TP_FRAME_DATA_LEN_MAX 512
-#define TP_HEADER                                                              \
-  { 0xEB, 0x90, 0xEB, 0x90 }
 
-const uint8_t tp_header[] = TP_HEADER;
+const uint8_t tp_header[] = {0xEB, 0x90, 0xEB, 0x90};
 
-typedef enum : uint8_t {
+enum {
+  TP_STATUS_NETWORK_ENCODING = 0x01,
+  TP_STATUS_ENTROPY_ENCODING = 0x02,
+};
+enum {
   TP_ADDRESS_MASTER,
   TP_ADDRESS_SLAVE,
-} address_t;
-typedef enum : uint8_t {
-  TP_FRAME_TYPE_REQUEST = 1,
-  TP_FRAME_TYPE_ACK = 3,
+};
+enum {
+  TP_FRAME_TYPE_QUERY = 1,
+  TP_FRAME_TYPE_SEND = 3,
+  TP_FRAME_TYPE_ACK = 4,
   TP_FRAME_TYPE_NACK = 5,
-} frame_type_t;
+  TP_FRAME_TYPE_RECV = 7,
+};
+typedef uint8_t address_t;
+typedef uint8_t frame_type_t;
 typedef uint32_t n_file_t;
 typedef uint32_t n_frame_t;
 typedef uint16_t data_len_t;
+typedef uint64_t status_t;
 typedef struct {
   address_t address;
   frame_type_t frame_type;
-  n_file_t n_file;
-  n_frame_t n_frame;
+  union {
+    status_t status;
+    struct {
+      n_file_t n_file;
+      n_frame_t n_frame;
+    };
+  };
   uint16_t check_sum;
 } frame_t;
 typedef struct {
@@ -40,22 +52,16 @@ typedef struct {
   uint8_t data[TP_FRAME_DATA_LEN_MAX];
   uint16_t check_sum;
 } data_frame_t;
-typedef struct {
-  uint8_t *addr;
-  size_t len;
-} data_t;
-
-size_t frame2bit_stream(frame_t frame, uint8_t *bit_stream);
-frame_t *bit_stream2frame(uint8_t *bit_stream);
 
 ssize_t send_frame(int, frame_t *);
 ssize_t receive_frame(int, frame_t *);
 ssize_t send_data_frame(int, data_frame_t *);
 ssize_t receive_data_frame(int, data_frame_t *);
-ssize_t data_frame_to_data_len(data_frame_t *, n_frame_t);
-void data_frames_to_data(data_frame_t *, n_frame_t, uint8_t *, size_t);
-void data_to_data_frames(data_frame_t *, n_frame_t, uint8_t *, size_t);
-ssize_t data_to_yuv420(uint8_t *, uint8_t *, uint8_t *, size_t);
+
+size_t data_frame_to_data_len(data_frame_t *, n_frame_t);
+void data_frames_to_data(data_frame_t *, n_frame_t, uint8_t *);
+void data_to_data_frames(uint8_t *, size_t, data_frame_t *);
+ssize_t data_to_yuv420(uint8_t *, uint8_t **, uint8_t **, size_t);
 void entropy_to_gmm(uint16_t *, gmm_t *);
 
 __END_DECLS
