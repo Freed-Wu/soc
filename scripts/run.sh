5b93cfcae4b8502aa4c0a8fc954077202f507577
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-12-04 17:08:53 +0800

    :children_crossing: Improve syslog()'s message

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,7 @@ cleanup() {
 trap cleanup EXIT
 
 socat pty,rawer,link=/tmp/ttyS0 pty,rawer,link=/tmp/ttyS1 &
-journalctl -tslave0 -tmaster -fn0 &
+SYSTEMD_COLORS=1 journalctl -tslave0 -tmaster -fn0 &
 # must wait journalctl to finish
 sleep 0.01
 main -dv &> /dev/null &
