ca5132e9433596bd24f02f4a83489e6596fda808
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-01-10 20:05:34 +0800

    :hammer: Improve scripts/burn.sh

--- scripts/burn.sh
+++ scripts/burn.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$(readlink -f "$0")")/.." || exit 1
 
-cp images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr /run/media/$USER/${1:-BOOT}
+boot=${1:-/run/media/$USER/BOOT}
+root=${2:-/run/media/$USER/root}
+cp images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr "$boot"
 # use sudo to avoid wrong privilege
-sudo rm -r /run/media/$USER/${2:-root}/*
-sudo tar vxaCf /run/media/$USER/${2:-root} images/linux/rootfs.tar.gz
+sudo rm -r "$root"/*
+sudo tar vxaCf "$root" images/linux/rootfs.tar.gz
 sync
