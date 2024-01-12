16a16b1cbf74bf0393f869d191b560ef40129d82
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-01-12 23:41:11 +0800

    :rewind: Revert ":bug: Fix wrong tty names"
    This reverts commit 86bb1e3511284c81ca8155606c9d0cd37a6e6536.

--- project-spec/meta-user/recipes-apps/autostart/src/main.c
+++ project-spec/meta-user/recipes-apps/autostart/src/main.c
@@ -43,7 +43,7 @@ static status_t get_status(n_file_t n_file) {
 }
 
 static void init_opt(opt_t *opt) {
-  opt->tty = SLAVE_TTY;
+  opt->tty = MASTER_TTY;
   opt->weight = "/usr/share/autostart/weight.bin";
   opt->quantization_coefficience = "/usr/share/autostart/quantify.bin";
 #ifdef DRY_RUN
