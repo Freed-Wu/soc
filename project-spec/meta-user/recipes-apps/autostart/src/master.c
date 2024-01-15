e58a7c4d8bc4d6bff06da94bd4f83a95d3f22952
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-01-15 16:37:00 +0800

    :truck: Move receive_data_frames()

--- project-spec/meta-user/recipes-apps/autostart/src/master.c
+++ project-spec/meta-user/recipes-apps/autostart/src/master.c
@@ -188,25 +188,6 @@ static void query_status(int send_fd, frame_t *output_frame, int send_timeout,
   }
 }
 
-static n_frame_t receive_data_frames(int recv_fd,
-                                     data_frame_t *input_data_frames,
-                                     frame_t input_frame, n_frame_t sum,
-                                     int timeout) {
-  data_frame_t data_frame;
-  for (n_frame_t _ = 0; _ < sum; _++) {
-    ssize_t n = receive_data_frame(recv_fd, &data_frame, timeout);
-    n_frame_t id = n_frame_to_id(data_frame.n_frame, input_frame.n_frame);
-    if (n <= 0 || data_frame.n_file != input_frame.n_file ||
-        id >= input_frame.n_frame || data_frame.flag != TP_FLAG_DATA ||
-        memcmp(data_frame.header, tp_header, sizeof(tp_header)))
-      continue;
-    memcpy(&input_data_frames[id], &data_frame, sizeof(data_frame));
-  }
-  // update sum
-  sum = count_unreceived_data_frames(input_data_frames, input_frame.n_frame);
-  return sum;
-}
-
 int main(int argc, char *argv[]) {
   opt_t opt;
   openlog(NULL, LOG_CONS | LOG_PERROR, 0);
