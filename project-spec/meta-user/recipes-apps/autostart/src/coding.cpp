37805f2339e6e289caafdd4bdb5f418cd8385b04
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-05-02 22:12:19 +0800

    :art: Refactor coding.cpp
    Co-authored-by: lxz <2263836119@qq.com>
    Co-authored-by: KBowei <97340226+KBowei@users.noreply.github.com>

--- project-spec/meta-user/recipes-apps/autostart/src/coding.cpp
+++ project-spec/meta-user/recipes-apps/autostart/src/coding.cpp
@@ -3,34 +3,29 @@
  * 需要求出每一个的概率的原因是需要总的频率和
  * 如果不求出就很难准确求出
  */
-#include <iostream>
-#include <math.h>
+#include <syslog.h>
 
 #include "arithmetic_coding.h"
 #include "coding.h"
-
-#define OUTPUT "/tmp/output.bin"
-
-double normal_cdf(double index, mean_t mean, std_t std) {
-  return 1.0 / 2 * (1 + erf((index - mean) / std / sqrt(2)));
-}
 
 extern "C" size_t coding(gmm_t *gmm, uint16_t *symbol, size_t len,
                          uint8_t *bits, uint32_t low_bound,
                          uint32_t high_bound) {
 
-  uint16_t m_probs[3] = {gmm->prob1, gmm->prob2, gmm->prob3};
-  uint16_t m_means[3] = {gmm->mean1, gmm->mean2, gmm->mean3};
-  uint16_t m_stds[3] = {gmm->std1, gmm->std2, gmm->std3};
+  syslog(LOG_NOTICE, "begin coding");
   uint32_t freqs_resolution = gmm->freqs_resolution;
-  GmmTable freqs(m_probs, m_means, m_stds, freqs_resolution, low_bound,
-                 high_bound);
-
   BitOutputStream bout(bits);
   CountingBitOutputStream c_bit_out(bout);
   ArithmeticEncoder enc(c_bit_out);
 
-  for (size_t k = 0; k < len; k++) {
+  gmm_t *ggmm = gmm;
+  for (size_t k = 0; k < len; k++, ggmm++) {
+    uint16_t m_probs[3] = {ggmm->prob1, ggmm->prob2, ggmm->prob3};
+    uint16_t m_means[3] = {ggmm->mean1, ggmm->mean2, ggmm->mean3};
+    uint16_t m_stds[3] = {ggmm->std1, ggmm->std2, ggmm->std3};
+    GmmTable freqs(m_probs, m_means, m_stds, freqs_resolution, low_bound,
+                   high_bound);
+
     int index = symbol[k];
 
     uint32_t total_freqs = freqs.total_freqs;
@@ -43,10 +38,6 @@ extern "C" size_t coding(gmm_t *gmm, uint16_t *symbol, size_t len,
   enc.finish();
   c_bit_out.close();
   size_t size = bout.size;
+  syslog(LOG_NOTICE, "end coding");
   return size;
-
-  // TODO: 解码端还没写（解码端需要频率表）
-  // std::fstream file_bin_read("tmp.bin", std::ios::in | std::ios::binary);
-  // BitInputStream bit_in(file_bin_read);
-  // ArithmeticDecoder dec(bit_in);
 }
