8d8262e645524dca31de4cb189efbe985af4290d
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-05-03 12:06:25 +0800

    :technologist: Add a script to merge config

---
+++ scripts/config.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S perl -pi
+# scripts/config.pl /the/path/of/source/config project-spec/configs/config
+unless ( $#ARGV < 0 ) {
+    unless ( $_ =~ '^#' ) {
+        /^([^=]*)(?{ $k = $^N })=(.*)(?{ $v = $^N })/;
+        $config{$k} = $v;
+    }
+    next;
+}
+while ( my ( $k, $v ) = each %config ) {
+    s/$k=.*/$k=$v/;
+}
