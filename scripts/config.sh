0486e923899ac6031046ea7677db2c3f83fa5a85
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-05-10 22:22:26 +0800

    :hammer: Improve scripts/config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -27,9 +27,6 @@ petalinux-config -crootfs --silentconfig
 petalinux-create -t apps -n autostart --enable --force
 petalinux-create -t modules -n axi-tangxi --enable --force
 # reset changes of `--force`
-git restore project-spec/meta-user/recipes-apps/autostart
-git clean -fd project-spec/meta-user/recipes-apps/autostart
-rm -rf project-spec/meta-user/recipes-apps/*.old
-git restore project-spec/meta-user/recipes-modules
-git clean -fd project-spec/meta-user/recipes-modules
-rm -rf project-spec/meta-user/recipes-modules/*.old
+git restore project-spec/meta-user/{recipes-apps/autostart,recipes-modules}
+git clean -fd project-spec/meta-user/{recipes-apps/autostart,recipes-modules}
+rm -rf project-spec/meta-user/recipes-{app,module}s/*.old
