cd5a5e4629f263396c0b398581ffed40fc9796f7
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-05-13 19:53:11 +0800

    :bug: Add -f for scripts/burn.sh

--- scripts/burn.sh
+++ scripts/burn.sh
@@ -6,6 +6,6 @@ boot=${1:-/run/media/$USER/BOOT}
 root=${2:-/run/media/$USER/root}
 cp images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr "$boot"
 # use sudo to avoid wrong privilege
-sudo rm -r "$root"/*
+sudo rm -rf "$root"/*
 sudo tar vxaCf "$root" images/linux/rootfs.tar.gz
 sync
