c6fd937b663d6b6947bc03dbc38bd327388b77c5
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-07-02 00:19:27 +0800

    :hammer: Improve scripts/copy-project.sh

--- scripts/copy-project.sh
+++ scripts/copy-project.sh
@@ -3,14 +3,12 @@ set -e
 cd "$(dirname "$(readlink -f "$0")")/.."
 pwd=$PWD
 
-[[ -n $1 ]] || set -- ../soc
-
-cd "$(dirname "$1")"
-basename="$(basename "$1")"
+cd "$(dirname "${1:-../soc}")"
+basename="$(basename "${1:-../soc}")"
 petalinux-create -tproject -n"$basename" --template zynqMP
 cd "$basename"
 cp -r "$pwd/.git" .
 git restore .
 
-install -Dm644 "$pwd"/project-spec/hw-description/system.xsa -t project-spec/hw-description
+install -Dm644 "${2:-"$pwd"/project-spec/hw-description/system.xsa}" -t project-spec/hw-description
 install -Dm644 "$pwd"/project-spec/meta-user/recipes-apps/autostart/assets/bin/{weight,cdf,exp}.bin -t project-spec/meta-user/recipes-apps/autostart/assets/bin
