8c5db31fe164b5f6089f6846a00a33c861b2e701
xiongzhuang liang <71314916+QQ2263836119@users.noreply.github.com>
2024-07-23 22:50:35 +0800

    Update coding.cpp

--- project-spec/meta-user/recipes-apps/autostart/src/coding.cpp
+++ project-spec/meta-user/recipes-apps/autostart/src/coding.cpp
@@ -3,51 +3,39 @@
  * 需要求出每一个的概率的原因是需要总的频率和
  * 如果不求出就很难准确求出
  */
-#include <syslog.h>
-
+#include <syslog.h>         
+#include <cstring>
+#include <cstdint>
+#include <string>
+#include <stddef.h> 
+#include <ctime>
+#include <dirent.h>
+#include <random>
+#include "coding.h"  
 #include "arithmetic_coding.h"
-#include "coding.h"
-// 统计时间
-#include <time.h>
 
-bool write_bin(uint8_t *data_addr, size_t size, char file[]) {
-  std::ofstream outputFile(file, std::ios::binary);
+extern "C" size_t coding(gmm_t* gmm,int16_t* trans_addr,size_t trans_len,uint8_t* data_addr,int16_t low_bound,int16_t high_bound){
+	BitOutputStream bout(data_addr);	
+	ArithmeticEncoder enc(32, bout);
 
-  if (!outputFile.is_open()) {
-    std::cerr << "Error opening file." << std::endl;
-    return false;
-  }
+	uint32_t freqs_resolution=1000000;
+	EncTable freqs_table(freqs_resolution,low_bound,high_bound);
 
-  outputFile.write(reinterpret_cast<char *>(data_addr), size);
-  outputFile.close();
-  return true;
-}
+	for(size_t i=0;i<trans_len;i++,gmm++){
+		int m_probs[3]={gmm->prob1, gmm->prob2,  gmm->prob3};
+		int m_means[3]={gmm->mean1, gmm->mean2, gmm->mean3};
+		int m_stds[3]={gmm->std1, gmm->std2, gmm->std3};
+		freqs_table.update(m_probs,m_means,m_stds);
 
-extern "C" size_t coding(gmm_t *gmm, int16_t *trans_addr, size_t trans_len,
-                         uint8_t *data_addr, uint32_t low_bound,
-                         uint32_t high_bound, uint32_t freqs_resolution) {
-  BitOutputStream bout(data_addr);
-  CountingBitOutputStream c_bit_out(bout);
-  ArithmeticEncoder enc(c_bit_out);
+		int symbol = trans_addr[i];
+		freqs_table.get_bound(symbol);
+		int total_freqs=freqs_table.total_freqs,symlow=freqs_table.sym_low,symhigh=freqs_table.sym_high;
+		enc.write(total_freqs, symlow, symhigh);
+	}
 
-  EncTable freqs_table(freqs_resolution, low_bound, high_bound);
+	enc.finish();
+	bout.finish();
+	size_t size=bout.size;
 
-  for (size_t i = 0; i < trans_len; i++, gmm++) {
-    uint16_t m_probs[3] = {gmm->prob1, gmm->prob2, gmm->prob3};
-    uint16_t m_means[3] = {gmm->mean1, gmm->mean2, gmm->mean3};
-    uint16_t m_stds[3] = {gmm->std1, gmm->std2, gmm->std3};
-    freqs_table.update(m_probs, m_means, m_stds);
-
-    int symbol = trans_addr[i];
-    freqs_table.get_bound(symbol);
-    uint32_t total_freqs = freqs_table.total_freqs,
-             symlow = freqs_table.sym_low, symhigh = freqs_table.sym_high;
-    enc.write(total_freqs, symlow, symhigh, symbol);
-  }
-
-  enc.finish();
-  c_bit_out.close();
-  size_t size = bout.size;
-
-  return size;
+	return size;
 }
