80fd7132f805e0c8ae145dda022e9ac45bcad6c0
xiongzhuang liang <71314916+QQ2263836119@users.noreply.github.com>
2024-08-07 21:36:08 +0800

    Update coding.cpp

--- project-spec/meta-user/recipes-apps/autostart/src/coding.cpp
+++ project-spec/meta-user/recipes-apps/autostart/src/coding.cpp
@@ -12,33 +12,42 @@
 #include <random>
 #include <stddef.h>
 #include <string>
-#include <syslog.h>
 
-extern "C" size_t coding(gmm_t *gmm, int16_t *trans_addr, size_t trans_len,
-                         uint8_t *data_addr, int16_t low_bound,
-                         int16_t high_bound) {
-  BitOutputStream bout(data_addr);
-  ArithmeticEncoder enc(32, bout);
+CodingResult codings(gmm_t* gmms[SUB_CNT], int16_t* datas[SUB_CNT], size_t* lens, int gmm_scale)
+{
+    size_t sum_len = accumulate(lens, lens + SUB_CNT, 0);
+    uint8_t* bins = (uint8_t*)malloc((sum_len*8)*sizeof(uint8_t));
 
-  uint32_t freqs_resolution = 1000000;
-  EncTable freqs_table(freqs_resolution, low_bound, high_bound);
+    BitOutputStream bout(bins + SUB_CNT*2*sizeof(int16_t));  // 从第52=SUB_CNT*2*2B开始
+    ArithmeticEncoder enc(32, bout);
 
-  for (size_t i = 0; i < trans_len; i++, gmm++) {
-    int m_probs[3] = {gmm->prob1, gmm->prob2, gmm->prob3};
-    int m_means[3] = {gmm->mean1, gmm->mean2, gmm->mean3};
-    int m_stds[3] = {gmm->std1, gmm->std2, gmm->std3};
-    freqs_table.update(m_probs, m_means, m_stds);
+    for (int i = 0; i < SUB_CNT; i++) {
+        int len = lens[i];
+        gmm_t* gmm = gmms[i];
+        int16_t* data = datas[i];
+        int16_t xmax = *max_element(data, data + len);
+        int16_t xmin = *min_element(data, data + len);
+        *reinterpret_cast<int16_t*>(bins + i*2*sizeof(int16_t)) = xmin;
+        *reinterpret_cast<int16_t*>(bins + (i*2+1)*sizeof(int16_t)) = xmax;
 
-    int symbol = trans_addr[i];
-    freqs_table.get_bound(symbol);
-    int total_freqs = freqs_table.total_freqs, symlow = freqs_table.sym_low,
-        symhigh = freqs_table.sym_high;
-    enc.write(total_freqs, symlow, symhigh);
-  }
+        if (xmin >= xmax) continue;
+        EncTable freqs_table(1000000, xmin, xmax);
+        freqs_table.scale_pred = gmm_scale;
+        for (int p = 0; p < len; p++, gmm++) {
+            int m_probs[3] = {gmm->prob1, gmm->prob2, gmm->prob3};
+            int m_means[3] = {gmm->mean1, gmm->mean2, gmm->mean3};
+            int m_stds[3] = {gmm->std1, gmm->std2, gmm->std3};
+            freqs_table.update(m_probs, m_means, m_stds);
 
-  enc.finish();
-  bout.finish();
-  size_t size = bout.size;
+            freqs_table.get_bound(data[p]);
+            int total_freqs = freqs_table.total_freqs, symlow = freqs_table.sym_low, symhigh = freqs_table.sym_high;
+            enc.write(total_freqs, symlow, symhigh);
+        }
+    }
 
-  return size;
+    enc.finish();
+    bout.finish();
+
+    CodingResult result = {bins, bout.size + SUB_CNT*2 * sizeof(int16_t)};
+    return result;
 }
