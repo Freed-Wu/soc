cb6b64c9a5a35af99b2f7d81a6d72c564d84add9
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-08-07 22:57:22 +0800

    :ambulance: Fix false

--- scripts/scp-and-burn.sh
+++ scripts/scp-and-burn.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 mkdir -p images/linux
 scp ${1:-_server:Desktop/soc/}images/linux/image.ub ${1:-_server:Desktop/soc/}images/linux/BOOT.BIN ${1:-_server:Desktop/soc/}images/linux/boot.scr ${1:-_server:Desktop/soc/}images/linux/rootfs.tar.gz images/linux
-shift || false
+shift || true
 [[ $# == 0 ]] && set -- "/run/media/$USER"/*
 exec scripts/burn.sh "$@"
