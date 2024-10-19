fd5e7600aa121108b637039b7dfdaca511acfcaa
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-10-20 03:01:20 +0800

    :hammer: Update scripts/burn.sh to support initramfs again

--- scripts/burn.sh
+++ scripts/burn.sh
@@ -2,7 +2,8 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-config="$(grep CONFIG_SUBSYSTEM_SDROOT_DEV project-spec/configs/config || true)"
+config="$(grep CONFIG_SUBSYSTEM_ROOTFS_EXT4=y project-spec/configs/config || true)"
+[[ -n $config ]] && config="$(grep CONFIG_SUBSYSTEM_SDROOT_DEV project-spec/configs/config || true)"
 config="${config#*\"}"
 config="${config%\"}"
 
@@ -12,7 +13,7 @@ if [[ $config == /dev/mmcblk0p2 ]]; then
 	# use sudo to avoid wrong privilege
 	sudo install -Dm644 images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr images/linux/rootfs.tar.gz -t "$root${3:-/home/root}"
 # sd
-elif [[ $config == /dev/mmcblk0p1 ]]; then
+elif [[ $config == /dev/mmcblk1p2 ]]; then
 	sudo rm -rf "$root"/*
 	sudo tar vxaCf "$root" images/linux/rootfs.tar.gz
 	boot=${1:-/run/media/$USER/BOOT}
