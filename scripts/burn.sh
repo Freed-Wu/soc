e06c9372dc013f3ececba3f38749080fa65e3fd9
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-10-20 02:37:26 +0800

    :hammer: Update scripts/burn.sh to support initramfs

--- scripts/burn.sh
+++ scripts/burn.sh
@@ -7,12 +7,18 @@ config="${config#*\"}"
 config="${config%\"}"
 
 root=${2:-/run/media/$USER/root}
+# emmc
 if [[ $config == /dev/mmcblk0p2 ]]; then
 	# use sudo to avoid wrong privilege
 	sudo install -Dm644 images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr images/linux/rootfs.tar.gz -t "$root${3:-/home/root}"
-else
+# sd
+elif [[ $config == /dev/mmcblk0p1 ]]; then
 	sudo rm -rf "$root"/*
 	sudo tar vxaCf "$root" images/linux/rootfs.tar.gz
+	boot=${1:-/run/media/$USER/BOOT}
+	cp images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr "$boot"
+# initramfs
+else
 	boot=${1:-/run/media/$USER/BOOT}
 	cp images/linux/image.ub images/linux/BOOT.BIN images/linux/boot.scr "$boot"
 fi
